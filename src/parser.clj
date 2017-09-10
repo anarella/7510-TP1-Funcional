d6767d1dbdc7e2ef631a4abbf2ada27f182f6556
Anarella <anarellanicoletta@gmail.com>
2017-09-10 14:51:41 -0300

    Rule-Fact-Syntax working fine. Should add some more validations though

--- src/parser.clj
+++ src/parser.clj
@@ -6,7 +6,11 @@
 (def rule-indicator ":-")
 (def opening-parentheses "(")
 (def closing-parentheses ")")
-(def minus-one -1)
+(def opening-parentheses-char \()
+(def closing-parentheses-char \))
+(def rule-indicator-length 2)
+(def not-found -1)
+(def initial-position 0)
 
 (defn remove-spaces
   "Receives a string and returns it containing no spaces."
@@ -31,16 +35,43 @@
   [fact]
   (let [opening-parentheses-index (.indexOf fact opening-parentheses)]
     (if
-      (and (> opening-parentheses-index minus-one) (>= (.indexOf fact closing-parentheses) opening-parentheses-index))
-      true false)
-    )
+      (and (> opening-parentheses-index not-found)
+           (>= (.indexOf fact closing-parentheses) opening-parentheses-index))
+      true false))
+  )
+
+(defn validate-left-side
+  "Receives the left side of a rule and checks the syntax of it, returning its
+  correspondent boolean value."
+  [left-string]
+  (let [opening-parentheses-index (.indexOf left-string opening-parentheses)]
+    (if
+      (and (> opening-parentheses-index not-found)
+           (>= (.indexOf left-string closing-parentheses) opening-parentheses-index))
+      true false))
+  )
+
+(defn validate-right-side
+  "Receives the right side of a rule and checks the syntax of it, returning its
+  correspondent boolean value."
+  [right-string]
+  (let [opening-parentheses-count
+        (count(filter #(= % opening-parentheses-char)(seq right-string)))
+        closing-parentheses-count
+        (count(filter #(= % closing-parentheses-char)(seq right-string)))]
+    (if (= opening-parentheses-count closing-parentheses-count) true false))
   )
 
 (defn check-rule-syntax
   "Receives a rule from the input and validates whether its syntax is correct or not,
   returning its correspondent boolean value."
   [rule]
-  false
+  (let [indicator-position (.indexOf rule rule-indicator)
+        left-side (subs rule initial-position indicator-position)
+        right-side (subs rule (+ indicator-position rule-indicator-length))]
+    (if
+      (and (validate-left-side left-side) (validate-right-side right-side))
+      true false))
   )
 
 (defn check-line-syntax
@@ -53,6 +84,7 @@
   [line]
   (if
     (not (str/ends-with? line dot-character)) false
-    (if (str/includes? line rule-indicator) (check-rule-syntax line)
+    (if (str/includes? line rule-indicator)
+      (check-rule-syntax line)
       (check-fact-syntax line)))
   )
